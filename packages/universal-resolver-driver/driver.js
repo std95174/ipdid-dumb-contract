5841b5b02a3781079f9f2be9ea2c005431feea2e
Frank Wang <frankwang95174@gmail.com>
2021-04-26 22:59:39 +0800

    update driver

--- packages/universal-resolver-driver/driver.js
+++ packages/universal-resolver-driver/driver.js
@@ -1,22 +1,32 @@
 const Koa = require('koa');
+const Router = require('@koa/router');
 const fs = require('fs');
 const path = require('path');
 const axios = require('axios');
 const { ethers } = require("ethers");
 
 const app = new Koa();
+const router = new Router();
 
 const provider = new ethers.providers.JsonRpcProvider(); // default: localhost:8545
 const contractAddress = fs.readFileSync(path.join(__dirname, "..", "hardhat", "artifacts", "IpDid.address"), "utf-8")
 const contractJSON = JSON.parse(fs.readFileSync(path.join(__dirname, "..", "hardhat", "artifacts", "contracts", "IpDid.sol", "IpDid.json"), "utf-8"))
 const contractABI = contractJSON.abi;
 
-// response
-app.use(async ctx => {
-    const did = "0xe371c5123e0131AbBbC7BA60c27896049C6cb892";
-    const contract = new ethers.Contract(contractAddress, contractABI, provider)
+router.get('/:did', async (ctx, next) => {
+    try {
+        const did = ctx.params.did;
+        const contract = new ethers.Contract(contractAddress, contractABI, provider)
+        ctx.body = { didDoc: JSON.parse(await contract.dids(did)) };
+    } catch (error) {
+        ctx.body = { errMsg: error };
+    }
 
-    ctx.body = JSON.parse(await contract.dids(did));
 });
 
-app.listen(3001);
+app
+    .use(router.routes())
+    .use(router.allowedMethods())
+    .listen(3001);
+
+console.log(`did resolver is listening on: http://localhost:3001 ...`);
