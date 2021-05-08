cc9b94d668456ebc9abb723ef6d3d91c39ed89df
Frank Wang <frankwang95174@gmail.com>
2021-05-09 01:14:15 +0800

    update index.js

--- packages/universal-resolver-driver/index.js
+++ packages/universal-resolver-driver/index.js
@@ -22,36 +22,63 @@ const testDidDocument = {
         "https://www.w3.org/ns/did/v1",
         "https://identity.foundation/EcdsaSecp256k1RecoverySignature2020/lds-ecdsa-secp256k1-recovery2020-0.0.jsonld"
     ],
-    id: "did:tw:0x03fdd57adec3d438ea237fe46b33ee1e016eda6b585c3e27ea66686c2ea5358479",
+    id: "did:ipdid:0x03fdd57adec3d438ea237fe46b33ee1e016eda6b585c3e27ea66686c2ea5358479",
     verificationMethod: [
         {
-            id: "did:tw:0x03fdd57adec3d438ea237fe46b33ee1e016eda6b585c3e27ea66686c2ea5358479#controller",
+            id: "did:ipdid:0x03fdd57adec3d438ea237fe46b33ee1e016eda6b585c3e27ea66686c2ea5358479#controller",
             type: "EcdsaSecp256k1RecoveryMethod2020",
-            controller: "did:tw:0x03fdd57adec3d438ea237fe46b33ee1e016eda6b585c3e27ea66686c2ea5358479",
+            controller: "did:ipdid:0x03fdd57adec3d438ea237fe46b33ee1e016eda6b585c3e27ea66686c2ea5358479",
             blockchainAccountId: "0xF3beAC30C498D9E26865F34fCAa57dBB935b0D74@eip155:1"
         },
         {
-            id: "did:tw:0x03fdd57adec3d438ea237fe46b33ee1e016eda6b585c3e27ea66686c2ea5358479#controllerKey",
+            id: "did:ipdid:0x03fdd57adec3d438ea237fe46b33ee1e016eda6b585c3e27ea66686c2ea5358479#controllerKey",
             type: "EcdsaSecp256k1VerificationKey2019",
-            controller: "did:tw:0x03fdd57adec3d438ea237fe46b33ee1e016eda6b585c3e27ea66686c2ea5358479",
+            controller: "did:ipdid:0x03fdd57adec3d438ea237fe46b33ee1e016eda6b585c3e27ea66686c2ea5358479",
             publicKeyHex: "0x03fdd57adec3d438ea237fe46b33ee1e016eda6b585c3e27ea66686c2ea5358479"
         }
     ],
     authentication: [
-        "did:tw:0x03fdd57adec3d438ea237fe46b33ee1e016eda6b585c3e27ea66686c2ea5358479#controller",
-        "did:tw:0x03fdd57adec3d438ea237fe46b33ee1e016eda6b585c3e27ea66686c2ea5358479#controllerKey"
+        "did:ipdid:0x03fdd57adec3d438ea237fe46b33ee1e016eda6b585c3e27ea66686c2ea5358479#controller",
+        "did:ipdid:0x03fdd57adec3d438ea237fe46b33ee1e016eda6b585c3e27ea66686c2ea5358479#controllerKey"
     ],
     assertionMethod: [
-        "did:tw:0x03fdd57adec3d438ea237fe46b33ee1e016eda6b585c3e27ea66686c2ea5358479#controller",
-        "did:tw:0x03fdd57adec3d438ea237fe46b33ee1e016eda6b585c3e27ea66686c2ea5358479#controllerKey"
+        "did:ipdid:0x03fdd57adec3d438ea237fe46b33ee1e016eda6b585c3e27ea66686c2ea5358479#controller",
+        "did:ipdid:0x03fdd57adec3d438ea237fe46b33ee1e016eda6b585c3e27ea66686c2ea5358479#controllerKey"
     ]
 }
 
+const testDidResolutionMetadata = {
+    duration: 1417,
+    identifier: "did:ipdid:0xtestaddr",
+    pattern: "^(did:ipdid:.+)$",
+    driverUrl: "http://bba-did-driver:8080/1.0/identifiers/",
+    didUrl: {
+        didUrlString: "did:ipdid:0xtestaddr",
+        did: {
+            didString: "did:ipdid:0xtestaddr",
+            method: "bba",
+            methodSpecificId: "0xtestaddr",
+            parseTree: null,
+            parseRuleCount: null
+        },
+        path: "",
+        query: null,
+        fragment: null,
+        parameters: {},
+        parseTree: null,
+        parseRuleCount: null
+    }
+}
+
+const testDidDocumentMetadata = {}
+
 router.get('/1.0/identifiers/:did', async (ctx, next) => {
     let did = ctx.params.did;
-    if (did == "0xtestaddr") {
+    if (did == "0xtestaddr" || did == "did:ipdid:0xtestaddr") {
         ctx.body = {
-            didDocument: testDidDocument
+            didDocument: testDidDocument,
+            didResolutionMetadata: testDidResolutionMetadata,
+            didDocumentMetadata: testDidDocumentMetadata
         }
         return
     }
@@ -59,6 +86,7 @@ router.get('/1.0/identifiers/:did', async (ctx, next) => {
         console.log(did);
         const contract = new ethers.Contract(contractAddress, contractABI, provider);
         const cid = await contract.dids(did);
+        console.log(`cid: ${cid}`);
         if (cid.length == 0) {
             ctx.status = 404;
             ctx.body = {
@@ -70,18 +98,24 @@ router.get('/1.0/identifiers/:did', async (ctx, next) => {
 
         // get did document from ipfs
         try {
-            const client = createClient('https://ipfs.infura.io:5001')
-            const result = await client.cat(cid);
+            // const client = createClient('https://ipfs.infura.io:5001')
+            // const result = await client.cat(cid);
+            const result = await axios.default.get(`https://ipfs.infura.io:5001/api/v0/block/get?arg=${cid}`);
+            console.log(result.data);
 
-            let didDocument;
-            for await (const item of result) {
-                didDocument = (item.toString());
-            }
+
+
+            // let didDocument;
+            // for await (const item of result) {
+            //     didDocument = (item.toString());
+            // }
 
             ctx.body = {
                 did: did,
                 cid: cid,
-                didDocument: JSON.parse(didDocument)
+                didDocument: result.data,
+                didResolutionMetadata: testDidResolutionMetadata,
+                didDocumentMetadata: testDidDocumentMetadata
             };
         } catch (error) {
             ctx.body = {
